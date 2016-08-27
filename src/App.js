baa98f4ab6e33dbae17ccccccf842f2e5b0d693e
Deonis Peretyagin <karudo@gmail.com>
2016-08-27 14:16:45 +0600

    react-router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
+import {Router, Route, browserHistory} from 'react-router'
+
+
+import Layout from './Layout';
+
+import First from './First';
+import Second from './Second';
+
+class App extends React.Component {
   render() {
     return (
-        <div className="container">
-            <h1 className="title">
-                Bulma test
-            </h1>
-        </div>
+        <Router history={browserHistory}>
+            <Route path="/" component={Layout}>
+                <Route path="first" component={First}/>
+                <Route path="second" component={Second}/>
+            </Route>
+        </Router>
     );
   }
 }
---
+++ src/Layout.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import {Link} from 'react-router';
+
+export default function Layout(props) {
+    return (
+        <div className="container">
+            <h1 className="title">
+                Bulma test
+            </h1>
+            <nav className="nav">
+                <div className="nav-left">
+                    <Link to="/first" className="nav-item is-tab">first</Link>
+                    <Link to="/second" className="nav-item">second</Link>
+                </div>
+            </nav>
+            {props.children}
+        </div>
+    );
+}

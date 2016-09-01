111873f7a62784813f6dca9b89150888f97dd36b
Deonis Peretyagin <karudo@gmail.com>
2016-09-01 08:54:52 +0600

    models WIP2

--- src/pages/Model.js
+++ src/pages/Model.js
@@ -5,10 +5,19 @@ import {connect} from 'react-redux';
 
 class ModelPage extends React.Component {
   render() {
-    return <div>190</div>;
+    console.log(this.props);
+    return <div onClick={e => this.props.dispatch({type: 'INC'})}>190</div>;
   }
 }
 
 export default connect(
-
+  state => {
+    console.log(state);
+    return {};
+  },
+  dispatch => ({dispatch}),
+  (q, w, e) => {
+    console.log(1, q, 2, w, 3, e);
+    return {...w};
+  }
 )(ModelPage);
